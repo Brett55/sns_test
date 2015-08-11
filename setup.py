62461699fddfe5356ea40e265ee64d80cc01ce4a
brett55 <bjcull2@gmail.com>
2015-08-11 13:56:08 -0600

    ready for pypi

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
   description = 'SNS utils',
   author = 'Brett Cullen',
   author_email = 'brett.cullen@digitalglobe.com',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  url = 'https://github.com/Brett55/sns_test', # use the URL to the github repo
+  download_url = 'https://github.com/Brett55/sns_test/tarball/0.1', # I'll explain this in a second
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   classifiers = [],
 )
